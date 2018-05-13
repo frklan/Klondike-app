85c50b91c9193ba98dce840ac0d7e37ffd6eeef7
Fredrik <fredrik.k.andersson@gmail.com>
2018-05-13 17:41:38 +0200

    Fixed error in Access-Control-Allow-Headers
    Allowing correct content type.

--- src/server/routes/api/v1/api.js
+++ src/server/routes/api/v1/api.js
@@ -26,7 +26,7 @@ router.get('/getaeonnetstat', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'application/json');
     return res.json(aeonNetworkStat);
   })
   .catch(function (error) {
