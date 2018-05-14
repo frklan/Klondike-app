06bd801a13efdf2fac20448ff9b4e71d3615e9a8
Fredrik <fredrik.k.andersson@gmail.com>
2018-05-14 22:31:35 +0200

    Api always sending access-control headers
    Did not always send correct access-control headers to client which resutlet in browser errors when accessing the api

--- src/server/routes/api/v1/api.js
+++ src/server/routes/api/v1/api.js
@@ -2,11 +2,16 @@ var express = require('express');
 var router = express.Router();
 const axios = require("axios");
 
+function sendAccessControlHeader(res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'application/json');
+}
+
 /**
  * Will cache response from chainradar for 10 seconds,
  * 
  */
-
 let aeonNetworkStat = '';
 let aeonLastUpdated = 0;
 
@@ -14,8 +19,10 @@ router.get('/getaeonnetstat', function(req, res, next) {
   let d = new Date();
   let currentTime = d.getTime();
 
+  sendAccessControlHeader(res);
+
   if(currentTime - aeonLastUpdated < 10000) {
-    return res.json(aeonNetworkStat);
+    return res.json(JSON.stringify(aeonNetworkStat));
   }
 
   axios.get('https://chainradar.com/api/v1/aeon/status')
@@ -23,16 +30,11 @@ router.get('/getaeonnetstat', function(req, res, next) {
     aeonNetworkStat = response.data;
     aeonLastUpdated = d.getTime();
 
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Headers', 'application/json');
-    return res.json(aeonNetworkStat);
+    return res.json(JSON.stringify(aeonNetworkStat));
   })
   .catch(function (error) {
     res.end(503);
   });
 });
-
 
 module.exports = router;
