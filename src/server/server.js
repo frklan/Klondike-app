f79d0e05f24a7a9a457c84d5b9ccdfa3b517479b
Fredrik <fredrik.k.andersson@gmail.com>
2018-05-15 21:49:42 +0200

    Simplified 404 handler
    - always return a json error when resource not found

--- src/server/server.js
+++ src/server/server.js
@@ -34,22 +34,15 @@ var apiV1 = require('./routes/api/v1/api.js')
 server.use('/api/v1', apiV1)
 
 
-// catch 404 and forward to error handler
-server.use(function(req, res, next) {
-  var err = new Error('Not Found' + req);
-  err.status = 404;
-  next(err);
+server.use(function(req, res, next){
+  res.status(404);
+  res.send({ error: 'resource not found' });
+  next();
 });
 
-// error handler
-server.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+function wrapAsync(fn) {
+  return function(req, res, next) {
+    fn(req, res, next).catch(next);
+  };
+}
 module.exports = server;
